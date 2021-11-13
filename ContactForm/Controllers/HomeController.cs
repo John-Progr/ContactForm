0d32371111e2d166ca140734daefe7c688900c68
John-Progr <62017772+John-Progr@users.noreply.github.com>
2021-11-13 12:16:24 +0200

    Add project files.

---
+++ ContactForm/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using ContactForm.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ContactForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserRepository UR=new UserRepository("File\\users.txt");
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Contact(User u)
+        {
+            //here the form sends the data to controller with http post request 
+            UR.saveUser(u);
+
+            //the controller form the model 
+            //the controller writes the data to file
+            //the controller returns 200ok
+            Thread.Sleep(8000);
+            ViewBag.Message = "OK";
+         
+
+
+            return View();
+
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ContactForm/Models/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactForm.Models
+{
+    public class UserRepository
+    {
+
+        String Directory { get; set; }
+        public UserRepository(String Directory)
+        {
+            this.Directory = Directory;
+        }
+        
+
+
+
+        public  async void saveUser(User user)
+        {
+            string line = user.Fullname + "," + user.Phone + "," + user.Email + "," + user.Message;
+            
+
+            using StreamWriter file = new(Directory, append: true);
+
+            await file.WriteLineAsync(line);
+
+        }
+
+
+       
+
+    }
+}
